180b41e1671b0a4ea5585acc5ae83d7a99d2150f
vilmacio <vilmaciomoura@gmail.com>
2024-10-14 16:25:57 -0300

    initial implementation

---
+++ src/app.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './resources/users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const isSQLite = configService.get<string>('DB_TYPE') === 'sqlite';
+        return {
+          type: isSQLite ? 'sqlite' : 'postgres',
+          host: isSQLite ? undefined : configService.get<string>('DB_HOST'),
+          port: isSQLite ? undefined : configService.get<number>('DB_PORT'),
+          username: isSQLite ? undefined : configService.get<string>('DB_USERNAME'),
+          password: isSQLite ? undefined : configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_DATABASE'),
+          entities: [join(__dirname, '**/*.entity{.ts,.js}')],
+          synchronize: true,
+        };
+      },
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('v1')
+
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(4000);
+}
+bootstrap();
